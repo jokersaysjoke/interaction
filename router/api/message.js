9351c982fdf7d0af8315e388cbf47a7b0973cb84
ChunYi <jokersaysjoke@gmail.com>
2023-06-06 03:47:52 +0800

    clean code

--- router/api/message.js
+++ router/api/message.js
@@ -26,28 +26,6 @@ msgAPI.get('/message', async(req, res)=>{
 
 })
 
-// msgAPI.post('/message', async(req, res)=>{
-//     try {
-//         const sql=`
-//         INSERT INTO MESSAGE (CHATROOM_ID, IMAGE_URL, CREATED_AT, USER_ID, CONTENT)
-//         VALUES (?,?,?,?,?)
-//         `;
-//         const data=req.body;
-//         const room=data.room;
-//         const avatar=data.avatar;
-//         const time=data.time;
-//         const user=data.user;
-//         const msg=data.msg;
-
-//         await pool.promise().query(sql, [room, avatar, time, user, msg]);
-//         return res.status(200).json({'ok': true});
-        
-//     } catch (error) {
-//         console.error(err);
-//         return res.status(500).json({"error":true, "message":"Database error"});
-//     }
-// });
-
 msgAPI.delete('/message', async(req, res)=>{
     try {
         let sql=`
