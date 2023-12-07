23daaae5cac71ab288f79c9dff32b834ed41a1ac
ChunYi <jokersaysjoke@gmail.com>
2023-12-07 18:51:20 +0800

    update TaipeiTime local to zh-TW

--- router/models/currentTime.js
+++ router/models/currentTime.js
@@ -1,5 +1,5 @@
 function getTaipeiTime() {
-    return new Date().toLocaleString(undefined, { timeZone: 'Asia/Taipei', hour12: false }).replace(',', '');
+    return new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei', hour12: false }).replace(',', '');
 }
 
 module.exports = { getTaipeiTime };
